ffe514e4637e9bd8100dab559c6a9c39b51af7bb
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-30 11:28:27 -0300

    Ao enviar SMS aparece um broadcastReceiver

--- app/src/main/java/br/com/fiap/broadcastreceiverenotification/MeuBroadcast.java
+++ app/src/main/java/br/com/fiap/broadcastreceiverenotification/MeuBroadcast.java
@@ -3,7 +3,9 @@ package br.com.fiap.broadcastreceiverenotification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class MeuBroadcast extends BroadcastReceiver {
@@ -12,8 +14,29 @@ public class MeuBroadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, R.string.broadcast_recebido, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, R.string.broadcast_recebido, Toast.LENGTH_SHORT).show();
 
         //Log.i("MeuLOG", "dsadsadsadsa");
+
+        Bundle myBundle = intent.getExtras();
+        SmsMessage[] messages = null;
+        String strMessage = "";
+        if (myBundle != null) {
+            Object[] pdus = (Object[]) myBundle.get("pdus");
+            messages = new SmsMessage[pdus.length];
+            for (int i = 0; i < messages.length; i++) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    String format = myBundle.getString("format");
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                strMessage += "SMS From: " + messages[i].getOriginatingAddress();
+                strMessage += " : ";
+                strMessage += messages[i].getMessageBody();
+                strMessage += "\n";
+            }
+            Toast.makeText(context, strMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
